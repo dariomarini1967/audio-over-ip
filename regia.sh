b7d19bb7ecddda93200937eb48c9d16554900d61
Dario Marini <marinidario1967@gmail.com>
2022-02-05 21:00:06 +0100

    Added jack connections for "regia" Enhancement in regia.sh

--- regia.sh
+++ regia.sh
@@ -1,13 +1,13 @@
 #! /usr/bin/bash -x
 
 ip_regia=$(ip -br a | grep UP | head -1 | sed -E 's/.+UP +//g' | sed -E 's/\/.+$//g')
-ip_palco=192.168.1.8
-utente_regia=marini
+ip_palco=dario-pc
 
 killall zita-j2n zita-n2j
 sleep 2
-zita-n2j --buff 100 --jname da_palco $ip_regia 4321 >/dev/null 2>&1 &
-zita-j2n --jname a_palco $ip_palco 4322 >/dev/null 2>&1 &
+zita-n2j --buff 30 --jname da_palco $ip_regia 4321 2>&1 >>da_palco.log &
+zita-j2n --jname a_palco $ip_palco 4322 2>&1 >>a_palco.log &
+
 
 
 
