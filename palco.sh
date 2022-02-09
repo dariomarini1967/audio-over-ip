5c5b8dd4a12f439eb69b73d5548e832d3073741a
Dario Marini <marinidario1967@gmail.com>
2022-02-09 23:15:14 +0100

    review of host detection in scripts

--- palco.sh
+++ palco.sh
@@ -1,13 +1,23 @@
-#! /usr/bin/bash 
+#! /usr/bin/bash
 
+echo "killing all jack clients"
 $(dirname $0)/kill_jack_clients.sh
 
-host_palco=$(host $(hostname)|head -1|sed  -E 's/^.+ //g')
-host_regia=lenovo
+host_palco=$(ip -br a | awk '($2=="UP"){gsub("/.+$","",$3);print $3}')
+#host_palco=192.168.1.16
+echo "host palco (this PC) is $host_palco"
+
+#host_regia=lenovo
+host_regia=192.168.0.100
+echo "host regia (remote PC) is $host_regia"
+
 sleep 2
 
 nohup zita-j2n --jname a_regia $host_regia 4321 2>&1 >>a_regia.log &
+sleep 1
 nohup zita-n2j --buff 30 --jname da_regia $host_palco 4322 2>&1 >>da_regia.log 2>&1 &
+
+exit
 
 # aggiunge X18 come seconda scheda audio
 nohup alsa_out -j x18_rec -r 48000 -d hw:0  2>&1 >>x18_rec.log &
--- regia.sh
+++ regia.sh
@@ -1,13 +1,21 @@
 #! /usr/bin/bash
 
+echo "killing all jack clients"
 $(dirname $0)/kill_jack_clients.sh
 
-ip_regia=$(ip -br a | grep UP | head -1 | sed -E 's/.+UP +//g' | sed -E 's/\/.+$//g')
-ip_palco=dario-pc
+#host_palco=dario-pc
+host_palco=192.168.0.101
+echo "host palco (remote PC) is $host_palco"
+
+host_regia=$(ip -br a | awk '($2=="UP"){gsub("/.+$","",$3);print $3}')
+#host_regia=192.168.1.8
+echo "host regia (this PC) is $host_regia"
+
 
 sleep 2
-zita-n2j --buff 30 --jname da_palco $ip_regia 4321 2>&1 >>da_palco.log &
-zita-j2n --jname a_palco $ip_palco 4322 2>&1 >>a_palco.log &
+
+zita-n2j --buff 30 --jname da_palco $host_regia 4321 2>&1 >>da_palco.log &
+zita-j2n --jname a_palco $host_palco 4322 2>&1 >>a_palco.log &
 
 
 
