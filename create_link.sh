f94c32c691451ecbff7714558553b0adce607c46
Dario Marini <marinidario1967@gmail.com>
2022-05-02 12:22:10 +0200

    config for PALCO should be ok

--- create_link.sh
+++ create_link.sh
@@ -1,5 +1,12 @@
 #! /bin/bash
 
+location=$1
+if [[ "$location" != "palco" && "$location" != "regia" ]]
+then
+	echo "Usage: $0 [palco|regia]"
+	exit 1
+fi
+
 curr_dir=$(readlink -f $(dirname $0))
 if [ ! -e ../.config/rncbc.org/QjackCtl.conf ]
 then
@@ -10,7 +17,7 @@ if [ ! -L ../.config/rncbc.org/QjackCtl.conf ]
 then
 	cp -p ../.config/rncbc.org/QjackCtl.conf .
 	cd ../.config/rncbc.org/
-	ln -vsf $curr_dir/QjackCtl.conf .
+	ln -vsf $curr_dir/QjackCtl_$location.conf .
 else
 	echo symlink already created
 fi
---
+++ start_pulseaudio.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+# should be useless
+systemctl --user start pulseaudio.socket pulseaudio.service
