714709a2ead98f10d1da8f364039350038ad5efa
Dario Marini <marinidario1967@gmail.com>
2022-05-06 22:30:47 +0200

    from REGIA Lisp and heaphone volume cano now be invoked

---
+++ init_regia.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/bash
+
+curr_dir=$(dirname $0)
+palco_or_regia=$($curr_dir/palco_or_regia.pl)
+if [ "$palco_or_regia" != "regia" ]
+then
+	echo "$0 must be launched on REGIA pc"
+	exit 1
+fi
+
+ssh -Y marini@palco.local audio-over-ip/start_lsp.sh &
+jack_mixer -c $curr_dir/mixer_regia.xml cuffie &
