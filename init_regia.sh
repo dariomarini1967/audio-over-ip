009cf3a121c01e74a00701e49f8603032c3d7173
Dario Marini <marinidario1967@gmail.com>
2022-06-12 20:58:35 +0200

    further bug fixing in init_regia.sh

--- init_regia.sh
+++ init_regia.sh
@@ -1,12 +1,12 @@
 #! /usr/bin/bash
 
-curr_dir=$(dirname $0)
+curr_dir=$HOME/audio-over-ip
 palco_or_regia=$($curr_dir/palco_or_regia.pl)
 if [ "$palco_or_regia" != "regia" ]
 then
 	echo "$0 must be launched on REGIA pc"
 	exit 1
 fi
-host_palco=$(egrep "^host_palco" ADDRESSES.cfg|cut -d"=" -f2)
+host_palco=$(egrep "^host_palco" $curr_dir/ADDRESSES.cfg|cut -d"=" -f2)
 ssh -Y tecnici@$host_palco audio-over-ip/start_lsp.sh &
 jack_mixer -c $curr_dir/mixer_regia.xml cuffie &
