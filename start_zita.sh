1aa6b8189a1f3f586d5f7b27c44560a2016ca600
Dario Marini <marinidario1967@gmail.com>
2022-09-23 00:19:08 +0200

    for starting zita hostname is no more required to contain "palco" or "regia"

--- start_zita.sh
+++ start_zita.sh
@@ -1,31 +1,17 @@
-#! /bin/bash
+#! /bin/bash -x
 
 cd $(dirname $0)
 
-if [ "$1" == "" ]
+palco_or_regia=$(./palco_or_regia.pl)
+if [[ $? -ne 0 ]]
 then
-	if [ ! -f /etc/avahi/avahi-daemon.conf ]
-	then
-		echo "/etc/avahi/avahi-daemon.conf not found; cannot understand if this PC is palco or regia"
-		exit 1
-	fi
-	is_palco=$(cat /etc/avahi/avahi-daemon.conf | awk '($1 ~ "^host-name *=.*palco"){print 2}')
-elif [ "$1" == "palco" ]
-then
-	is_palco=1
-elif [ "$1" != "regia" ]
-then
-	echo "Usage: $0 [palco|regia]"
 	exit 1
 fi
-
-###exec >> zita.log 2>&1
-echo $(date)" - is_palco is $is_palco"
 
 host_palco=$(egrep "^host_palco" ADDRESSES.cfg|cut -d"=" -f2)
 host_regia=$(egrep "^host_regia" ADDRESSES.cfg|cut -d"=" -f2)
 
-if [ $is_palco ]
+if [ "$palco_or_regia" == "palco" ]
 then
 	echo "starting zita for PALCO"
 	zita-j2n --jname a_regia $host_regia 4321 2>&1 >>a_regia.log &
