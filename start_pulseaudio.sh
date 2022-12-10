15a664623e7e4330c31173b0bdea288df7d29683
Dario Marini <marinidario1967@gmail.com>
2022-12-10 21:53:32 +0100

    starting pulse audio now creates also jack sink & source

--- start_pulseaudio.sh
+++ start_pulseaudio.sh
@@ -2,3 +2,7 @@
 
 # should be useless
 systemctl --user start pulseaudio.socket pulseaudio.service
+
+pactl load-module module-jack-sink channels=2
+pactl load-module module-jack-source channels=2
+
