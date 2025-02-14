c8b53f4cde98ccb656eaa8f8e1368e2f0f34fa8a
Dario Marini <marinidario1967@gmail.com>
2025-02-14 22:21:33 +0100

    added jack_connect_vsc.sh script to immediately connect 16 channels from Ardour to X32 USB input for virtual sound check

---
+++ jack_connect_vsc.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+jack_connect "ardour:Audio 1/audio_out 1" system:playback_1
+jack_connect "ardour:Audio 2/audio_out 1" system:playback_2
+jack_connect "ardour:Audio 3/audio_out 1" system:playback_3
+jack_connect "ardour:Audio 4/audio_out 1" system:playback_4
+jack_connect "ardour:Audio 5/audio_out 1" system:playback_5
+jack_connect "ardour:Audio 6/audio_out 1" system:playback_6
+jack_connect "ardour:Audio 7/audio_out 1" system:playback_7
+jack_connect "ardour:Audio 8/audio_out 1" system:playback_8
+jack_connect "ardour:Audio 9/audio_out 1" system:playback_9
+jack_connect "ardour:Audio 10/audio_out 1" system:playback_10
+jack_connect "ardour:Audio 11/audio_out 1" system:playback_11
+jack_connect "ardour:Audio 12/audio_out 1" system:playback_12
+jack_connect "ardour:Audio 13/audio_out 1" system:playback_13
+jack_connect "ardour:Audio 14/audio_out 1" system:playback_14
+jack_connect "ardour:Audio 15/audio_out 1" system:playback_15
+jack_connect "ardour:Audio 16/audio_out 1" system:playback_16
