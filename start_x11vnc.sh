083db7ceefa679752f60006a07c4f59c1fb10c71
Dario Marini <marinidario1967@gmail.com>
2026-03-01 15:28:47 +0100

    X11VNC launched from current user

--- start_x11vnc.sh
+++ start_x11vnc.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-nohup sudo /usr/bin/x11vnc -forever -display :0 -auth guess -ncache 10 -passwd automat 2>&1 >x11vnc.log &
+nohup /usr/bin/x11vnc -rfbport 5901 -forever -display :0 -auth guess -ncache 10 -passwd automat 2>&1 >x11vnc.log &
